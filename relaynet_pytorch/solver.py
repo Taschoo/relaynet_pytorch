b21a21c7d51c0811737798aa52a7fc47b2ce3e59
Taschoo <natascha.wolf@smail.th-koeln.de>
2024-07-17 03:26:09 +0200

    njsdxfc

--- relaynet_pytorch/solver.py
+++ relaynet_pytorch/solver.py
@@ -6,7 +6,7 @@ from torch.autograd import Variable
 from relaynet_pytorch.net_api.losses import CombinedLoss
 from torch.optim import lr_scheduler
 import os
-
+from tqdm import tqdm
 
 def per_class_dice(y_pred, y_true, num_class):
     avg_dice = 0
@@ -75,8 +75,8 @@ class Solver(object):
         iter_per_epoch = 1
         # iter_per_epoch = len(train_loader)
 
-        if torch.cuda.is_available():
-            model.cuda()
+        #if torch.cuda.is_available() and torch.version.cuda is not None:
+        #    model.cuda()
 
         print('START TRAIN.')
         curr_iter = 0
@@ -84,36 +84,30 @@ class Solver(object):
         create_exp_directory(exp_dir_name)
 
         for epoch in range(num_epochs):
-            for i_batch, sample_batched in enumerate(train_loader):
-                X = Variable(sample_batched[0])
-                y = Variable(sample_batched[1])
-                w = Variable(sample_batched[2])
+            with tqdm(total=len(train_loader), desc=f'Epoch {epoch + 1}/{num_epochs}', unit='batch') as pbar:
+                for i_batch, sample_batched in enumerate(train_loader):
+                    X = Variable(sample_batched[0])
+                    y = Variable(sample_batched[1])
+                    w = Variable(sample_batched[2])
 
-                if model.is_cuda:
-                    X, y, w = X.cuda(), y.cuda(), w.cuda()
+                    if model.is_cuda:
+                        X, y, w = X.cuda(), y.cuda(), w.cuda()
 
-                for iter in range(iter_per_epoch):
-                    curr_iter += iter
-                    optim.zero_grad()
-                    output = model(X)
-                    loss = self.loss_func(output, y, w)
-                    loss.backward()
-                    optim.step()
-                    if iter % log_nth == 0:
-                        self.train_loss_history.append(loss.data[0])
-                        print('[Iteration : ' + str(iter) + '/' + str(iter_per_epoch * num_epochs) + '] : ' + str(
-                            loss.data[0]))
+                    for iter in range(iter_per_epoch):
+                        curr_iter += iter
+                        optim.zero_grad()
+                        output = model(X)
+                        loss = self.loss_func(output, y, w)
+                        loss.backward()
+                        optim.step()
+                        if iter % log_nth == 0:
+                            self.train_loss_history.append(loss.item())
+                            print('[Iteration : ' + str(iter) + '/' + str(iter_per_epoch * num_epochs) + '] : ' + str(
+                                loss.item()))
 
+                    pbar.update(1)
 
-                #_, batch_output = torch.max(F.softmax(model(X),dim=1), dim=1)
-                #avg_dice = per_class_dice(batch_output, y, self.NumClass)
-                #print('Per class average dice score is ' + str(avg_dice))
-                # self.train_acc_history.append(train_accuracy)
-                #
-                # val_output = torch.max(model(Variable(torch.from_numpy(val_loader.dataset.X))), dim= 1)
-                # val_accuracy = self.accuracy(val_output[1], Variable(torch.from_numpy(val_loader.dataset.y)))
-                # self.val_acc_history.append(val_accuracy)
-            print('[Epoch : ' + str(epoch) + '/' + str(num_epochs) + '] : ' + str(loss.data[0]))
-            model.save('models/' + exp_dir_name + '/relaynet_epoch' + str(epoch + 1) + '.model')
-            scheduler.step()
+                print('[Epoch : ' + str(epoch) + '/' + str(num_epochs) + '] : ' + str(loss.item()))
+                model.save('models/' + exp_dir_name + '/relaynet_epoch' + str(epoch + 1) + '.model')
+                scheduler.step()
         print('FINISH.')
